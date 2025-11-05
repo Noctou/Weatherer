3a0b4248f219ed5df17d8020aa7cdeba1af8b168
Leon Bryan <leonard.bryan.email@gmail.com>
2025-11-05 20:47:43 +0800

    Hmmm...

---
+++ app/(root)/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import {useEffect} from "react";
+
+export default function Home() {
+
+  // useEffect(() => {
+  //   async function fetchWeather() {
+  //     const lat = 14.5995;
+  //     const lon = 120.9842;
+
+  //     const url = `${process.env.NEXT_PUBLIC_OPENWEATHER_BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}&units=metric`;
+
+  //     console.log("Fetching:", url);
+
+  //     try {
+  //       const res = await fetch(url);
+
+  //       if (!res.ok) {
+  //         throw new Error(`HTTP error! status: ${res.status}`);
+  //       }
+        
+  //       const data = await res.json();
+  //       console.log("Weather data:", data);
+  //     } catch (err) {
+  //         console.error("Error fetching API:", err);
+  //     }
+  //   }
+
+  //   fetchWeather();
+  // }, []);
+
+  return (
+    <div>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <h1 className="text-4xl font-bold font-mono">Hello</h1>
+      </div>
+    </div>
+  );
+}
